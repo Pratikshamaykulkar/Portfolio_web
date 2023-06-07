323a39e533a7fb56d1f4f0977226c6023f2a6af8
pratiksha <maykulkarpratiksha@gmail.com>
2023-06-07 08:21:56 +0530

    files added

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var emailLink = document.getElementById('emailLink');
+  
+    emailLink.addEventListener('click', function(event) {
+      alert('Email link clicked!');
+    });
+  });
+  
